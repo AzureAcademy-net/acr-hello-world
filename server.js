4f2a0ea7a42bf72f07bfc60012204b71222a2f22
Maarten de Vries <maar.10@hotmail.com>
2023-12-10 07:04:24 +0100

    Inital commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+const port = 80
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/plain'})
+  response.write('Hello ACR tasks\n')
+})
+
+server.listen(port)
+
+console.log(`Server running at http://localhost: ${port}`)
